9e3e28d1b919907321f62295d4f2d977c07ed0ed
Ajeet yadav <ajeetyadav164@gmail.com>
2022-07-01 19:36:42 +0530

    Docker File Added

---
+++ Jar-CICD/src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
@@ -0,0 +1,21 @@
+package com.codingprotocols.jarcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JarCicdApplication {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "<H1> Hello World </H1>";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarCicdApplication.class, args);
+    }
+
+}
---
+++ Jar-CICD/src/test/java/com/codingprotocols/jarcicd/JarCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingprotocols.jarcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
