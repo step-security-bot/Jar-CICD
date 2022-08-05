8ead375c02463197c2dbc2cbd1afd03e1e606a0c
Ajeet yadav <ajeetyadav164@gmail.com>
2022-08-05 15:22:26 +0530

    Testing ELK

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
 FROM openjdk:8-jdk-alpine
 COPY target/Jar-CICD-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
+
+
+
+#FROM openjdk:16-alpine3.13
+#
+#WORKDIR /app
+#
+#COPY .mvn/ .mvn
+#COPY mvnw pom.xml ./
+#RUN ./mvnw dependency:go-offline
+#COPY src ./src
+#EXPOSE 8080
+#CMD ["./mvnw", "spring-boot:run"]
--- src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
+++ src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
@@ -5,17 +5,53 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 @SpringBootApplication
 @RestController
 public class JarCicdApplication {
 
-    @GetMapping("/")
+    private static final Logger LOG = Logger.getLogger(JarCicdApplication.class.getName());
+
+
+    @GetMapping(value = "/")
     public String helloWorld() {
-        return "<H1> Hello World </H1>";
+        String response = "<H1> Hello World </H1>" + new Date();
+        LOG.log(Level.INFO, response);
+
+        return response;
     }
+
+    @GetMapping(value = "/exception")
+    public String exception() {
+        String response;
+        try {
+            throw new Exception("Exception has occured....");
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOG.error(e);
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString();
+            LOG.error("Exception - " + stackTrace);
+            response = stackTrace;
+        }
+
+        return response;
+    }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(JarCicdApplication.class, args);
     }
+
+
 
 }
