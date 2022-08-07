90eab9a0c90d4e7dc9ef49e62ff27e0ffefc4a4e
Ajeet yadav <ajeetyadav164@gmail.com>
2022-08-07 11:34:27 +0530

    Update kill_container_docker.sh

--- scripts/kill_container_docker.sh
+++ scripts/kill_container_docker.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-docker rm -f jarcicd || true
+CNAME=jarcicd
+#CNAME=$CONTAINER_NAME-$CODE_VERSION
+if [ "$(docker ps -qa -f name=$CNAME)" ]; then
+    echo ":: Found container - $CNAME"
+    if [ "$(docker ps -q -f name=$CNAME)" ]; then
+        echo ":: Stopping running container - $CNAME"
+        docker stop $CNAME;
+    fi
+    echo ":: Removing stopped container - $CNAME"
+    docker rm -f $CNAME;
+fi
+
+#docker rm -f jarcicd || true
