c992684e6a124cd1f672eb36924a7ec2e8be5711
Ajeet yadav <ajeetyadav164@gmail.com>
2022-08-07 16:06:40 +0530

    Codedeploy

--- src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
+++ src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
@@ -20,7 +20,7 @@ public class JarCicdApplication {
 
     @GetMapping(value = "/")
     public String helloWorld() {
-        String response = "<H1> Hello World - Load balancer testing -1 Target  </H1>" + new Date();
+        String response = "<H1> Hello World   </H1>" + new Date();
         LOG.log(Level.INFO, response);
 
         return response;
