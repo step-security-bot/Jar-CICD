ee0b6bf21f56c5ac84aba531160e742be9643e37
Ajeet yadav <ajeetyadav164@gmail.com>
2022-08-11 22:36:29 +0530

    Codedeploy

--- src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
+++ src/main/java/com/codingprotocols/jarcicd/JarCicdApplication.java
@@ -1,27 +1,25 @@
 package com.codingprotocols.jarcicd;
 
+import java.util.Date;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Date;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
 @SpringBootApplication
 @RestController
 public class JarCicdApplication {
 
-    private static final Logger LOG = Logger.getLogger(JarCicdApplication.class.getName());
-
+    private static final Logger LOG = LogManager.getLogger(JarCicdApplication.class);
 
     @GetMapping(value = "/")
     public String helloWorld() {
         String response = "<H1> Hello World   </H1>" + new Date();
-        LOG.log(Level.INFO, response);
+        LOG.log(Level.DEBUG, response);
 
         return response;
     }
@@ -30,28 +28,17 @@ public class JarCicdApplication {
     public String exception() {
         String response;
         try {
-            throw new Exception("Exception has occured....");
+            throw new Exception("Exception has occurred....");
         } catch (Exception e) {
             e.printStackTrace();
-            LOG.error(e);
-
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            String stackTrace = sw.toString();
-            LOG.error("Exception - " + stackTrace);
-            response = stackTrace;
+            LOG.log(Level.ERROR, e);
+            response = e.toString();
         }
 
         return response;
     }
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(JarCicdApplication.class, args);
     }
-
-
-
 }
